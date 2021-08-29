39e1e825e75ced4f52abd38b680d77e33ad9b2cf
Rish-Fish <rita.levkoy@gmail.com>
2021-08-29 18:02:08 +0300

    feat: add scroll-disable to body

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ let menuLeft = document.querySelector('.menu-left'); // накинуть сюд�
 let mobileMenuWrap = document.querySelector('.mobile-menu-wrap'); // накинуть .active (display:block)
 let faBarsToggle = document.querySelector('.fa-bars'); // накинуть сюда .disable (display:none)
 let faTimesToggle = document.querySelector('.fa-times'); //накинуть .active (display:block)
+let scrollDisable = document.getElementsByTagName('body'); //накинуть сюда .scroll-disable
+let scrollDisableGoToTop = document.querySelector('.to_top_container'); //накинуть сюда .disable
 
 faBarsToggle.addEventListener('click', function () {
   menuLeft.classList.toggle('menu-left-active');
   mobileMenuWrap.classList.toggle('active');
   faBarsToggle.classList.toggle('disable');
   faTimesToggle.classList.toggle('active');
+  document.body.classList.add('scroll-disable');
+  scrollDisableGoToTop.classList.toggle('disable');
 });
 
 faTimesToggle.addEventListener('click', function () {
@@ -19,4 +23,6 @@ faTimesToggle.addEventListener('click', function () {
   mobileMenuWrap.classList.remove('active');
   faTimesToggle.classList.remove('active');
   faBarsToggle.classList.remove('disable');
+  document.body.classList.remove('scroll-disable');
+  scrollDisableGoToTop.classList.remove('disable');
 });
